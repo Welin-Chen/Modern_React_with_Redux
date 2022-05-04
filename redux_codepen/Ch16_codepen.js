6f508a9bd519167b60471a5def73a81101f22c15
welin <a0929015591@gmail.com>
2022-05-04 22:43:07 +0800

    Ch.16: Oh We Go...To Redux!

---
+++ redux_codepen/Ch16_codepen.js
@@ -0,0 +1,86 @@
+console.clear();
+
+// People dropping off a form (Action Creator)
+const createPolicy = (name, amount) => {
+  return {
+    // Action (a form in our analoy)
+    type: "CREATE_POLICY",
+    payload: {
+      name: name,
+      amount: amount,
+    },
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name: name,
+    },
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect,
+    },
+  };
+};
+
+// Reducer (Departments!)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === "CREATE_CLAIM") {
+    // We care about this action (FORM!)
+    return [...oldListOfClaims, action.payload];
+  }
+
+  // we don't care the action (FORM!)
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === "CREATE_POLICY") {
+    return bagOfMoney + action.payload.amount;
+  }
+
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter((name) => name !== action.payload.name);
+  }
+
+  return listOfPolicies;
+};
+
+console.log(Redux);
+const { createStore, combineReducers } = Redux;
+
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies,
+});
+
+const store = createStore(ourDepartments);
+console.log(store);
+
+store.dispatch(createPolicy("Alex", 20));
+store.dispatch(createPolicy("Jim", 30));
+store.dispatch(createPolicy("Bob", 40));
+
+store.dispatch(createClaim("Alex", 120));
+store.dispatch(createClaim("Jim", 50));
+
+store.dispatch(deletePolicy("Bob"));
+
+console.log(store.getState());
